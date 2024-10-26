6381e4df8c8252128f0cfbc856d8e6a33be160b3
kbt060124 <kbt060124@gmail.com>
2024-10-26 16:17:32 +0900

    tailwindcss追加

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,7 +8,9 @@ function Home() {
             <nav>
                 <ul>
                     <li>
-                        <Link to="/studio">Studio</Link>
+                        <Link to="/studio">
+                            <div className="text-red-700">Studio</div>
+                        </Link>
                     </li>
                     <li>
                         <Link to="/warehouse">Warehouse</Link>
