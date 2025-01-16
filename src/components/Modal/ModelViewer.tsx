e9c14d338c228c492e9ec3cff6346a55655697b2
kbt060124 <kbt060124@gmail.com>
2025-01-16 13:50:05 +0900

    Warehouseとuserの紐づけ

--- src/components/Modal/ModelViewer.tsx
+++ src/components/Modal/ModelViewer.tsx
@@ -1,23 +1,21 @@
 import React from "react";
 import SceneComponent from "../SceneComponent";
 import { setupWarehouseScene } from "../../utils/sceneSetup";
+import { type Warehouse } from "./types";
 
-interface ModelViewerProps {
-    itemId: bigint;
-    warehouseId: bigint;
-}
-
-const ModelViewer: React.FC<ModelViewerProps> = ({ itemId, warehouseId }) => (
+const ModelViewer: React.FC<{ warehouse: Warehouse }> = ({ warehouse }) => (
     <div className="flex-1 min-h-[250px] sm:min-h-[300px] overflow-hidden">
         <SceneComponent
             antialias
             onSceneReady={(scene) =>
                 setupWarehouseScene(
                     scene,
-                    `https://test-fbx-upload.s3.ap-southeast-2.amazonaws.com/${itemId}.glb`
+                    `${import.meta.env.VITE_S3_URL}/warehouse/${
+                        warehouse.user_id
+                    }/${warehouse.id}/${warehouse.filename}`
                 )
             }
-            id={`canvas-${warehouseId}`}
+            id={`canvas-${warehouse.id}`}
             className="w-full h-full"
         />
     </div>
